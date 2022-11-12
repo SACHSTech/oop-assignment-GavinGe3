4d87acbe3688f7333f608631f5757d3e68353ff2
GavinGe3 <gavin.ge23@ycdsbk12.ca>
2022-11-12 13:55:55 -0500

    Created drink class and constructor.

--- src/Uber/Drink.java
+++ src/Uber/Drink.java
@@ -1,3 +1,18 @@
-public class Drink {
+package Uber;
+
+public abstract class Drink {
+    private String strItemName;
+    private String strSize;
+    private int intCost;
+    private int intCalories;
+    private int menuNumber;
+    
+    public Drink(String newName, String newSize, int newCost, int newCalories, int newMenuNumber){
+        strItemName = newName;
+        strSize = newSize;
+        intCost = newCost;
+        intCalories = newCalories;
+        menuNumber = newMenuNumber;
+    }
     
 }
