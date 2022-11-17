ee0c9e13fea2a268bb4e117a30cc78fa2442607f
GavinGe3 <gavin.ge23@ycdsbk12.ca>
2022-11-17 09:12:50 -0500

    Updated Main Class, added getOrder Method to Order

--- src/Uber/Main.java
+++ src/Uber/Main.java
@@ -11,12 +11,15 @@ public class Main {
         
         Menu newMenu = new Menu();
         Order newOrder = new Order();
+        int numFoods = 0;
+        int numDrinks = 0;
 
 
         System.out.println("Welcome to BigDonalds, the best fast food in the business.");
         System.out.println("Here is the preview of our menu.");
 
         while(true){
+
             newMenu.getInfo();
 
             System.out.println("Enter the Menu Number of your desired item.");
@@ -25,12 +28,22 @@ public class Main {
             if (currentOrder < 12){
                 newMenu.getFood(currentOrder).setInfo();
                 newOrder.addFoodToOrder(newMenu.getFood(currentOrder));
+                numFoods += 1;
             }
             if (currentOrder > 11){
                 newMenu.getDrink(currentOrder).setInfo();
                 newOrder.addDrinkToOrder(newMenu.getDrink(currentOrder));
+                numDrinks += 1;
             }
+
+            newOrder.getOrder(numFoods, numDrinks);
+
+            System.out.println("Enter 0 to complete your order. Enter 1 to continue ordering");
+            int continueOrder = scanner.nextInt();
+            if (continueOrder == 0){
+                break;
+            }
+
         }
-        
     }
 }
--- src/Uber/Order.java
+++ src/Uber/Order.java
@@ -12,8 +12,6 @@ public class Order {
         ArrayList<Drink> listOfDrinks = new ArrayList<Drink>();
         Customer customer;
         int totalCost;
-
-        
     }
 
     public void addFoodToOrder(Food newFood){
@@ -28,6 +26,26 @@ public class Order {
 
     public double getCost(){
         return totalCost;
+    }
+
+    public void getOrder(int intFoods, int intDrinks){
+        System.out.println("-----Order Summary-----");
+        System.out.println();
+        
+        for (int i = 0;i < intFoods; i++){
+            System.out.println("---Food Item " + i + " ---");
+            listOfFoods.get(i).getInfo();
+            System.out.println();
+        }
+        for (int i = 0;i < intDrinks; i++){
+            System.out.println("---Drink Item " + i + " ---");
+            listOfDrinks.get(i).getInfo();
+            System.out.println();
+        }
+
+        System.out.println("Your subtotal is: " + totalCost);
+
+
     }
 }
 
